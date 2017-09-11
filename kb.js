ce433175e866c4069729fefeee21360a2a2470fb
Mikulas Stec <mikulas.stec@nordlicht-it.com>
2017-09-11 13:25:19 +0200

    kb-new-someangularetc

--- kb.js
+++ kb.js
@@ -1,2 +1,8 @@
 PROXY NODE.JS NGINX
 https://www.exratione.com/2012/07/proxying-websocket-traffic-for-nodejs-the-present-state-of-play/
+
+https://www.packtpub.com/books/content/using-handlebars-express
+
+https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
+
+https://developer.okta.com/blog/2017/04/17/angular-authentication-with-oidc?utm_campaign=Syndication%3EGlobal%3Esep-05-homepage-tile-FY18Q3&utm_medium=display&utm_source=scotch-all-developer
